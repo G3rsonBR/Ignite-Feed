377bafef85566ef0d24bb6523781611775eded47
G3rsonBR <gersons.br13@gmail.com>
2024-06-15 18:58:47 -0300

    feat: Implentação da Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-function App() {
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 
+import "./global.css";
+import styles from "./App.module.css";
+
+export function App() {
   return (
     <>
-      <h1>Ignite: Ignite Feed Project</h1>
-    </>
-  )
-}
+      <Header />
 
-export default App
+      <div className={styles.wrapper}>
+        <aside>
+          SideBar
+        </aside>
+
+        <main>
+          <Post
+            author="Gerson"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil maxime reiciendis tenetur velit laborum nisi cupiditate nam aperiam veniam, vitae optio ipsam, recusandae, perspiciatis quam. Sint animi enim cumque esse."
+          />
+          <Post
+            author="Renata"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil maxime reiciendis tenetur velit laborum nisi cupiditate nam aperiam veniam, vitae optio ipsam, recusandae, perspiciatis quam. Sint animi enim cumque esse."
+          />
+        </main>
+      </div>
+    </>
+  );
+}
