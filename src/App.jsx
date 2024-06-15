98587525d9acede044cdabad92074ce8f1934036
G3rsonBR <gersons.br13@gmail.com>
2024-06-15 20:05:19 -0300

    feat: componente de post e estilização inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from "./components/Header";
 import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import "./global.css";
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
@@ -14,14 +14,8 @@ export function App() {
         <Sidebar />
 
         <main>
-          <Post
-            author="Gerson"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil maxime reiciendis tenetur velit laborum nisi cupiditate nam aperiam veniam, vitae optio ipsam, recusandae, perspiciatis quam. Sint animi enim cumque esse."
-          />
-          <Post
-            author="Renata"
-            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil maxime reiciendis tenetur velit laborum nisi cupiditate nam aperiam veniam, vitae optio ipsam, recusandae, perspiciatis quam. Sint animi enim cumque esse."
-          />
+          <Post />
+          <Post />
         </main>
       </div>
     </>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,37 @@
-export function Post(props) {
+import styles from "./Post.module.css";
+
+export function Post() {
   return (
-    <div>
-      <h2>{props.author}</h2>
-      <p>{props.content}</p>
-    </div>
-  )
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=40&w=200&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Usuário Legal</strong>
+            <span>Desenvolvedor Java Sr.</span>
+          </div>
+        </div>
+
+        <time title="15 de julho às 19h44" dateTime="2024-15-07 19:44:55">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>👉<a href="#">jane.design/doctorcare</a></p>
+        <p>
+          <a href="#">#novoprojeto</a>
+          <a href="#">#nlw</a> 
+          <a href="#">#rocketseat</a></p>
+      </div>
+    </article>
+  );
 }
